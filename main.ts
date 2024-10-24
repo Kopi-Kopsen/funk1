9b34040b74b90a1d5850ae9e756d5fc670a73d9b
Kopi-Kopsen <posnerla@thg-heilbronn.de>
2024-10-24 11:00:50 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,30 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    radio.sendString("RLF")
+    radio.sendString("L")
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    radio.sendString("F")
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    radio.sendString("R")
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendString("B")
 })
 radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+    if ("R" == receivedString) {
+        basic.showString("R")
+        MotionKit.turnRight(30)
+    } else if ("L" == receivedString) {
+        basic.showString("L")
+        MotionKit.turnLeft(30)
+    } else if ("F" == receivedString) {
+        basic.showString("F")
+        MotionKit.driveForwards(50)
+    } else if ("B" == receivedString) {
+        basic.showString("B")
+        MotionKit.driveBackwards(50)
+    }
 })
 basic.forever(function () {
-    radio.setGroup(89)
+    radio.setGroup(27)
 })
